2d60b08eadad97f972b588df5f66c765b56c0eaa
João P. Kiyoshi <kiyoshi33@gmail.com>
2023-03-21 15:22:15 -0300

    add product list component

---
+++ components/organisms/ProductList.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import ProductCard from '../molecules/ProductCard';
+
+type Props = {};
+
+const GridContainer = styled.section`
+	display: grid;
+	place-items: center;
+	grid-template-columns: repeat(auto-fill, minmax(min(216px, 100%), 1fr));
+	gap: 20px;
+	max-width: 1300px;
+	margin-inline: auto;
+	padding: 25px;
+`;
+
+const ProductList = (props: Props) => {
+	return (
+		<GridContainer>
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+		</GridContainer>
+	);
+};
+export default ProductList;
