6a039798d380a3182f92f7d3e1fcd8834d686734
João P. Kiyoshi <kiyoshi33@gmail.com>
2023-03-21 08:39:16 -0300

    add default styles

---
+++ styles/globalStyles.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
+  }
+
+  body {
+   /* font-family: 'Roboto', sans-serif; */
+   font-family: 'Comic Neue', cursive;
+
+    --orange: #ffb800;
+    --red: #aa0001;
+    --white: #f8f8f8;
+    --black: #161616;
+  }
+`;
