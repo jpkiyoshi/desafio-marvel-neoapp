b9f634f6faeb7ad057f6e451ab36399342958daa
João P. Kiyoshi <kiyoshi33@gmail.com>
2023-03-26 14:21:09 -0300

    fix build error 5

--- pages/comics/[id].tsx
+++ pages/comics/[id].tsx
@@ -1,4 +1,4 @@
-import { GetStaticPaths } from 'next';
+import { GetServerSideProps, GetStaticPaths } from 'next';
 import { GetStaticProps } from 'next';
 import ProductDetails from '@/components/organisms/ProductDetails';
 
@@ -26,26 +26,26 @@ const Comic = ({ data }: any) => {
 
 export default Comic;
 
-export const getStaticPaths: GetStaticPaths = async () => {
-	const res = await fetch(
-		`http://gateway.marvel.com/v1/public/comics?ts=1&apikey=${process.env.NEXT_PUBLIC_API_KEY}&hash=cfc29b20f1501cf633b27057de8fe8a1`
-	);
+// export const getStaticPaths: GetStaticPaths = async () => {
+// 	const res = await fetch(
+// 		`http://gateway.marvel.com/v1/public/comics?ts=1&apikey=${process.env.NEXT_PUBLIC_API_KEY}&hash=cfc29b20f1501cf633b27057de8fe8a1`
+// 	);
 
-	const data = await res.json();
+// 	const data = await res.json();
 
-	const ids = data?.data.results.map((comic: { id: number }) => comic.id);
+// 	const ids = data?.data.results.map((comic: { id: number }) => comic.id);
 
-	const paths = ids.map((id: number) => ({
-		params: { id: `${id}` },
-	}));
+// 	const paths = ids.map((id: number) => ({
+// 		params: { id: `${id}` },
+// 	}));
 
-	return {
-		paths,
-		fallback: true,
-	};
-};
+// 	return {
+// 		paths,
+// 		fallback: true,
+// 	};
+// };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
 	const res = await fetch(
 		`http://gateway.marvel.com/v1/public/comics/${params?.id}?ts=1&apikey=${process.env.NEXT_PUBLIC_API_KEY}&hash=cfc29b20f1501cf633b27057de8fe8a1`
 	);
@@ -55,6 +55,5 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 		props: {
 			data,
 		},
-		revalidate: 60,
 	};
 };
